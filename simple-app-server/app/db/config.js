daf68bc537913dd3fd01e7b6f69bbb33bfcc6550
Ganesh Kaila <ganesh.kaila@searce.com>
2021-10-08 15:16:33 +0530

    database log update

--- simple-app-server/app/db/config.js
+++ simple-app-server/app/db/config.js
@@ -1,11 +1,32 @@
 const Pool = require("pg").Pool
 
+let postgres_host
+
+if (process.env.POSTGRES_HOST.split(":")[1] == undefined) {
+	postgres_host = process.env.POSTGRES_HOST + ":" + process.env.POSTGRES_PORT
+} else {
+	postgres_host = process.env.POSTGRES_HOST
+}
+
+const connectionString =
+	"postgresql://" +
+	process.env.POSTGRES_USER +
+	":" +
+	process.env.POSTGRES_PASSWORD +
+	"@" +
+	postgres_host +
+	"/" +
+	process.env.POSTGRES_DB
+
 const pool = new Pool({
-	user: process.env.POSTGRES_USER,
-	host: process.env.POSTGRES_HOST,
-	database: process.env.POSTGRES_DB,
-	password: process.env.POSTGRES_PASSWORD,
-	port: process.env.POSTGRES_PORT
+	connectionString
+})
+
+pool.query("SELECT NOW()", (err, res) => {
+	if (err) {
+		reject(err)
+	}
+	console.log("Database connection successful.")
 })
 
 module.exports = pool
